64b6c8d541e58d5688a1de96e2b493e3d2c9eb38
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 00:11:55 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -3,12 +3,12 @@ package com.stackroute.domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component //indicate to resolver this is a class ,for this create a object 
 public class Movie {
 
     private Actor actor;
 
-    @Autowired
+    @Autowired  //handling nullpointer Exception
     public void setActor(Actor actor) {
         this.actor = actor;
     }
