41aaea82a5091b9a7b2ef46499839d2aaf9ca159
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 00:23:00 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,6 +15,18 @@ public class Main {
         Movie movie=context.getBean("movieBean", Movie.class);
         movie.displayInfo();
 
+        //display actor information using the XMLBean factory
+            XmlBeanFactory factory = new XmlBeanFactory (new ClassPathResource(MovieConfig.class));
+            Movie obj = factory.getBean("rammovie",Movie.class);
+            obj.displayinfo();
 
+            //display actor information using the beandefinition registry
+            DefaultListableBeanFactory factory1=new DefaultListableBeanFactory();
+            BeanDefinitionRegistry context1=new GenericApplicationContext(factory1);
+            BeanDefinitionReader reader=new XmlBeanDefinitionReader(context1);
+            reader.loadBeanDefinitions(MovieConfig.class);
+            Movie shivam=factory1.getBean("rammovie",Movie.class);
+            shivam.displayinfo();
+        
     }
 }
