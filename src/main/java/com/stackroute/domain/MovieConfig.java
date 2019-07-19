31e182a5f78604a18283500e762593da80b31896
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 10:28:12 +0530

    commited code

--- src/main/java/com/stackroute/domain/MovieConfig.java
+++ src/main/java/com/stackroute/domain/MovieConfig.java
@@ -3,11 +3,11 @@ package com.stackroute.domain;
 
 import org.springframework.context.annotation.*;
 
-
+//indicatin the file is conguration file or not
 @Configuration
 public class MovieConfig {
 
-    @Bean
+    @Bean //creating for bean
     public Actor actorBean(){
 
        return new Actor("ram","male",21);
