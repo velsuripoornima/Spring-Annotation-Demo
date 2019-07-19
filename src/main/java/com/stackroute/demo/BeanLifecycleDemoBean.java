c89a3e5608f88112836ff936a202071ed2a01f52
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 09:50:53 +0530

    Update BeanLifecycleDemoBean.java

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -6,17 +6,20 @@ import org.springframework.beans.factory.InitializingBean;
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
 
 
+    //inbuit method for disposal bean
     public void destroy() throws Exception {
 
         System.out.println("implements disposal bean.....");
     }
 
+    //inbuit method for initilizing bean
     public void afterPropertiesSet() throws Exception {
 
         System.out.println("implementing initializing bean ....");
 
     }
 
+    
     public void customInit(){
 
         System.out.println("custon initilization......");
