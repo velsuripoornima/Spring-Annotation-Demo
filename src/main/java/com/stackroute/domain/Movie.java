6c8ebfacef8c75ae3684f2eba6dcfdcab73cddbc
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-07-19 09:41:43 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -11,22 +11,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+////indicate to resolver this is a class ,for this create a object
 @Component
 public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
-    @Autowired
+    @Autowired  //handling nullpointer Exception
     private Actor actor;
 
     public Movie(){
 
     }
 
+     //constructor for actor class
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
 
-//   @Autowired
     public void setActor(Actor actor) {
         this.actor = actor;
     }
@@ -36,16 +37,19 @@ public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNam
         actor.actorInformation();
     }
 
+    //inbuilt method for applicationcontextaware
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
         System.out.println("implementing ApplicationContextAware .....");
     }
 
+    //inbuilt method for beanfactory aware
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
 
         System.out.println("Implementing beanfactoryAware .....");
     }
 
+    //inbuilt method for beannameaware
     public void setBeanName(String s) {
 
         System.out.println("Implementing beanNameAware.....");
